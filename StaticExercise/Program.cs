f85aad156a142855ee06de736dc2ace3f9b78aec
Andrea Schwenk <andreaschwenk3@gmail.com>
2022-04-01 10:17:15 -0500

    Completed To-Do

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConverter.DisplayTemp();
+            
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double UsersTempInput()
+        {
+            Console.WriteLine("This is a converter, please enter a temperature");
+            var userTempInput = double.Parse(Console.ReadLine());
+           return userTempInput;
+        }
+
+        public static void DisplayTemp()
+        {
+            var input = UsersTempInput();
+            Console.WriteLine($"{input} converted to Farehnheit is {CelsiusToFarenheit(input)}");
+            Console.WriteLine($"{input} converted to Celsius is {FarehnheitToCelsius(input)}");
+        }
+        public static double FarehnheitToCelsius(double farenheitTemp)
+        {
+            var celsius = (farenheitTemp - 32) / 1.8;
+            return celsius;
+        }
+
+        public static double CelsiusToFarenheit(double celsiusTemp)
+        {
+            var fahrenheit = (celsiusTemp * 9) / 5 + 32;
+            return fahrenheit;
+        }
+
+
+
+
+
+    }
+
+
+
+}
+
